7018c501c645b5968b3c6bf2072b772cb93badce
Alok Yadav <1032240842@tcetmumbai.in>
2026-04-19 12:43:13 +0530

    Initial commit - Todo List App

---
+++ code.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+//     int n;
+//     cout<<"Enter the size of array:";
+//     cin>>n;
+//     int a[n];
+//     cout<<"Enter the elements of an array";
+//     for(int i = 0; i<n ;i++)
+//     cin>>a[i];
+//     int target_value;
+//     cout<<"enter target value:";
+//     cin>>target_value;
+//    for(int i = 0; i<n-1; i++){
+//     for(int j = i+1; j<n; j++){
+//         if(a[i]+a[j]==target_value){
+//             cout<<i<<" "<<j<<endl;
+//             return 0;
+//         }
+// }
+//    }
+// cout<<"Not a proper array value taken"<<endl;
+// return 0;
+// int a[5]={3,7,2,9,5};
+// int size = 5;
+// int max_element = a[0];
+// int index = 0;
+// for(int i = 1; i<size; i++){
+//     if(a[i]>max_element){
+//         max_element=a[i];
+//         index = i;
+//     }
+// }
+// cout<<max_element<<endl;
+// cout<<index<<endl;
+// return 0;
+
+// 
+// int a[5]={1,2,3,4,1};
+// int size = 5;
+// int i = 0;
+// int j = size-1;
+// bool ispalindrome = true;
+// while(i<j){
+//     if(a[i]!=a[j]){
+//         ispalindrome = false;
+//        break;
+//     }
+//     i++; 
+//     j--;
+// }
+// if(ispalindrome){
+//     cout<<"Alok! this is palindrome"<<endl;
+// }
+// else{
+//     cout<<"Alok this is not palindrome"<<endl;
+// }
+// }
+// int arr[5]={2,4,6,5,4};
+// int *p;
+// p = &arr[4];
+// printf("%d",*(&arr+1));
+// }
+// 
+}
+
---
+++ todolist.js
@@ -0,0 +1,26 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerText = task;
+
+    // delete button
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+
+    deleteBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(deleteBtn);
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = ""; // clear input
+}
